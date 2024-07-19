fc365eaf3a6996b85e7fa71f907680cc6031b27f
Motomoto99 <motoshou39@icloud.com>
2024-07-19 16:33:34 +0900

    使用していないimportの削除

--- react-sample/src/index.js
+++ react-sample/src/index.js
@@ -3,14 +3,6 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import EventBasic from './components/EventBasic';
-import StateBasic from './samples/my-react/src/chap03/StateBasic';
-import books from './samples/my-react/src/chap03/books'
-import ForNest from './samples/my-react/src/chap03/ForNest';
-import SelectStyle from './samples/my-react/src/chap03/SelectStyle';
-import EventPoint from './samples/my-react/src/chap03/EventPoint';
-import EventKey from './samples/my-react/src/chap03/EventKey';
-import EventPassive from './samples/my-react/src/chap03/EventPassive';
 import StateForm from './samples/my-react/src/chap04/StateForm';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
