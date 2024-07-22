a5ad6cc179a0840665116d27150d897e801dbb0b
Motomoto99 <motoshou39@icloud.com>
2024-07-22 19:33:00 +0900

    Formについてとimmerについて

--- react-sample/src/index.js
+++ react-sample/src/index.js
@@ -4,11 +4,27 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import StateForm from './samples/my-react/src/chap04/StateForm';
+import StateFormUC from './samples/my-react/src/chap04/StateFormUC';
+import FormSelect from './samples/my-react/src/chap04/FormSelect';
+import FormTextarea from './samples/my-react/src/chap04/FormTextarea';
+import FormList from './samples/my-react/src/chap04/FormList';
+import FormFile from './samples/my-react/src/chap04/FormFile';
+import StateNest from './samples/my-react/src/chap04/StateNest';
+import StateNestImmer from './samples/my-react/src/chap04/StateNestImmer';
+import StateNestImmer2 from './samples/my-react/src/chap04/StateNestImmer2';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <>
     <StateForm />
+    <StateFormUC />
+    <FormTextarea />
+    <FormSelect />
+    <FormList />
+    <FormFile />
+    <StateNest />
+    <StateNestImmer />
+    <StateNestImmer2 />
   </>
 
 );
