5dc9a3a13510dcb674be5016dacf4faa8ed69241
Motomoto99 <motoshou39@icloud.com>
2024-07-25 21:26:40 +0900

    Stateの配列の操作

--- react-sample/src/index.js
+++ react-sample/src/index.js
@@ -3,28 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import StateForm from './samples/my-react/src/chap04/StateForm';
-import StateFormUC from './samples/my-react/src/chap04/StateFormUC';
-import FormSelect from './samples/my-react/src/chap04/FormSelect';
-import FormTextarea from './samples/my-react/src/chap04/FormTextarea';
-import FormList from './samples/my-react/src/chap04/FormList';
-import FormFile from './samples/my-react/src/chap04/FormFile';
-import StateNest from './samples/my-react/src/chap04/StateNest';
-import StateNestImmer from './samples/my-react/src/chap04/StateNestImmer';
-import StateNestImmer2 from './samples/my-react/src/chap04/StateNestImmer2';
+import StateTodo from './samples/my-react/src/chap04/StateTodo';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <>
-    <StateForm />
-    <StateFormUC />
-    <FormTextarea />
-    <FormSelect />
-    <FormList />
-    <FormFile />
-    <StateNest />
-    <StateNestImmer />
-    <StateNestImmer2 />
+    <StateTodo />
   </>
 
 );
