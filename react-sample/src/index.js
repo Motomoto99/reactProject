82430a7fbdb0c61ab90ed1221744ef6fa124cb76
Motomoto99 <motoshou39@icloud.com>
2024-08-26 21:59:40 +0900

    Route test

--- react-sample/src/index.js
+++ react-sample/src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import HookDeferredTransition from './components/chap07/HookDeferredTransition';
+import routesBasic from './components/chap08/routesBasic';
+import { RouterProvider } from 'react-router-dom';
+import routesLink from './components/chap08/routesLink';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <>
-    <HookDeferredTransition />
+    <RouterProvider router={routesLink} />
   </>
 );
 
