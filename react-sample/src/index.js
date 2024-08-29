3e4310e41529fd3c69010263f6a8c2bd3ad5bd94
Motomoto99 <motoshou39@icloud.com>
2024-08-29 12:28:57 +0900

    param test

--- react-sample/src/index.js
+++ react-sample/src/index.js
@@ -2,15 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import HookDeferredTransition from './components/chap07/HookDeferredTransition';
-import routesBasic from './components/chap08/routesBasic';
 import { RouterProvider } from 'react-router-dom';
-import routesLink from './components/chap08/routesLink';
+import routesParam from './components/chap08/routesParam';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <>
-    <RouterProvider router={routesLink} />
+    <RouterProvider router={routesParam} />
   </>
 );
 
