8568b292c66e9bdf2240929753de00327b6d0ebe
Motomoto99 <motoshou39@icloud.com>
2024-10-18 16:29:48 +0900

    update framework

--- original-app/src/components/Header.js
+++ original-app/src/components/Header.js
@@ -1,18 +1,39 @@
 import React from 'react'
 import { NavLink, Link } from 'react-router-dom'
+import { useState } from 'react'
+import { Home, Mail, Info, AccounTree } from '@mui/icons-material'
+import { Box, Button, Drawer, List, ListItem, ListItemButton, ListItemText, ListItemIcon } from '@mui/material'
+import MenuIcon from '@mui/icons-material/Menu'
+
+const program = [
+    { title: 'ホーム', href: '/', icon: Home },
+    { title: '寿命計算機', href: '/lifespan', icon: Info },
+]
 
 export default function Header() {
+    const [show, setShow] = useState(false)
+    const handleDraw = () => setShow(!show)
+
     return (
-        <header>
-            <div>
-                <h3>オリジナルプログラムスペース</h3>
-            </div>
-            <nav>
-                <ul>
-                    <li><Link to="/">ホーム</Link></li>
-                    <li><Link to="/lifespan">寿命計算機</Link></li>
-                </ul>
-            </nav>
-        </header>
+        <>
+            <Button onClick={handleDraw}>ドロワー</Button>
+            <Drawer anchor='left' open={show}>
+                <Box sx={{ height: '100vh' }} onClick={handleDraw}>
+                    <List>
+                        {program.map(obj => {
+                            const Icon = obj.icon
+                            return(
+                                <ListItem key={obj.title}>
+                                    <ListItemButton href={obj.href}>
+                                        <ListItemIcon><Icon/></ListItemIcon>
+                                        <ListItemText primary={obj.title} />
+                                    </ListItemButton>
+                                </ListItem>
+                            )
+                        })}
+                    </List>
+                </Box>
+            </Drawer>
+        </>
     )
 }
--- original-app/src/components/HomePage.js
+++ original-app/src/components/HomePage.js
@@ -3,7 +3,8 @@ import React from 'react'
 export default function HomePage() {
   return (
     <div>
-        <h3>HomePage</h3>
+        <h3>概要</h3>
+        <p>自分が作りたいと思った簡単なプログラムを気軽に作成して使えるように管理していく</p>
     </div>
   )
 }
