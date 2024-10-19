ef9080ff757d6bc504561bb876ea6c935805d79e
Motomoto99 <motoshou39@icloud.com>
2024-10-19 23:59:31 +0900

    components update

--- original-app/src/components/HomePage.js
+++ original-app/src/components/HomePage.js
@@ -3,8 +3,8 @@ import React from 'react'
 export default function HomePage() {
   return (
     <div>
-      <h2 className='text-2xl p-3 font-serif'>Home</h2>
-      <h3>概要</h3>
+      <h2 className='text-2xl p-3 font-serif font-bold'>Home</h2>
+      <h3 className='font-bold'>概要</h3>
       <p>自分が作りたいと思った簡単なプログラムを気軽に作成して使えるように管理していく</p>
     </div>
   )
