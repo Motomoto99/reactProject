dd122b8111ed01820ca17003f9b6f85f6b597fbd
Motomoto99 <motoshou39@icloud.com>
2024-10-19 23:58:58 +0900

    components update

--- original-app/src/components/HomePage.js
+++ original-app/src/components/HomePage.js
@@ -3,8 +3,9 @@ import React from 'react'
 export default function HomePage() {
   return (
     <div>
-        <h3>概要</h3>
-        <p>自分が作りたいと思った簡単なプログラムを気軽に作成して使えるように管理していく</p>
+      <h2 className='text-2xl p-3 font-serif'>Home</h2>
+      <h3>概要</h3>
+      <p>自分が作りたいと思った簡単なプログラムを気軽に作成して使えるように管理していく</p>
     </div>
   )
 }
---
+++ original-app/src/components/Lifespan.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Lifespan() {
+  return (
+    <div>Lifespan</div>
+  )
+}
