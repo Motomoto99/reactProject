8f34f3df590034ef1e18d268bc57da0022c6fdbe
Motomoto99 <motoshou39@icloud.com>
2024-10-19 22:51:03 +0900

    Tailwind CSS relationship

---
+++ original-app/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // JSXファイルを対象にする
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
