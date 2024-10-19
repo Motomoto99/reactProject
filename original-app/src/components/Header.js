13a42cff45c711710a9f184c27f331e03f109463
Motomoto99 <motoshou39@icloud.com>
2024-10-19 22:52:42 +0900

    Header CSS Indications

--- original-app/src/components/Header.js
+++ original-app/src/components/Header.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import { NavLink, Link } from 'react-router-dom'
 import { useState } from 'react'
 import { Home, Mail, Info, AccounTree } from '@mui/icons-material'
 import { Box, Button, Drawer, List, ListItem, ListItemButton, ListItemText, ListItemIcon } from '@mui/material'
@@ -16,16 +15,27 @@ export default function Header() {
 
     return (
         <>
-            <Button onClick={handleDraw}>ドロワー</Button>
+            <header className="text-gray-600 body-font bg-purple-300">
+                <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+                    <h2 className='text-2xl p-3 font-serif'>ORIGINAL APPLICATION</h2>
+                    <a className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full mr-3" viewBox="0 0 24 24">
+                            <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
+                        </svg>
+                        <Button variant="contained" color='secondary' onClick={handleDraw}>SELECT</Button>
+                    </a>
+                </div>
+            </header>
+            
             <Drawer anchor='left' open={show}>
                 <Box sx={{ height: '100vh' }} onClick={handleDraw}>
                     <List>
                         {program.map(obj => {
                             const Icon = obj.icon
-                            return(
+                            return (
                                 <ListItem key={obj.title}>
                                     <ListItemButton href={obj.href}>
-                                        <ListItemIcon><Icon/></ListItemIcon>
+                                        <ListItemIcon><Icon /></ListItemIcon>
                                         <ListItemText primary={obj.title} />
                                     </ListItemButton>
                                 </ListItem>
