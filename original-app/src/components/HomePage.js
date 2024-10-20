feca124d2b55e39ab4bc1aa2e38f9317b439d94e
Motomoto99 <motoshou39@icloud.com>
2024-10-20 22:14:43 +0900

    home adjust

--- original-app/src/components/HomePage.js
+++ original-app/src/components/HomePage.js
@@ -3,9 +3,11 @@ import React from 'react'
 export default function HomePage() {
   return (
     <div>
-      <h2 className='text-2xl p-3 font-serif font-bold'>Home</h2>
-      <h3 className='font-bold'>概要</h3>
+      <h2 className='text-2xl p-3 font-serif font-bold'>ホーム</h2>
+      <h3 className='font-bold text-xl'>概要</h3>
       <p>自分が作りたいと思った簡単なプログラムを気軽に作成して使えるように管理していく</p>
+      <br/>
+      <p><span className='font-bold'>SELECT</span>ボタンをクリックして気になるプログラムを選択してください</p>
     </div>
   )
 }
